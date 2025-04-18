766bfaedf4b820b32543e614faf96072b3c21383
marina kondratieva <55560721+mkondratieva@users.noreply.github.com>
2025-04-18 23:47:29 +0300

    Add files via upload

---
+++ myclass.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<ctime>
+class my{
+	int x=rand() ;
+	public: 
+	double method(const double &y){return x+y;}
+	double operator+(const double &y)const &{
+		return x+y;
+	}
+	friend double operator +(const double &, const  my &);
+};
+double operator +(const double &y, const  my &obj){ 
+	return obj+y;
+}
+
+int main(){
+srand(time(0));
+	my z; 
+std::cout<<my().method(0.1)<<"   "<<my()+-0.1<<"  "<<0.7+my();
+	return 0;
+}
